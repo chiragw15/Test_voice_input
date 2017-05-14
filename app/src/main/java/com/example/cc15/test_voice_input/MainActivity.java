d892bd3e2692d1a29de2f33994d3a2a438f65da3
cc15 <chiragwadhera15@gmail.com>
2017-05-14 16:08:18 +0530

    initial commit2

--- app/src/main/java/com/example/cc15/test_voice_input/MainActivity.java
+++ app/src/main/java/com/example/cc15/test_voice_input/MainActivity.java
@@ -56,43 +56,6 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private static final int SPEECH_REQUEST_CODE = 0;
-
-    // Create an intent that can start the Speech Recognizer activity
-    private void displaySpeechRecognizer() {
-        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
-                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-        intent.putExtra(RecognizerIntent.EXTRA_PARTIAL_RESULTS,true);
-// Start the activity, the intent will be populated with the speech text
-        startActivityForResult(intent, SPEECH_REQUEST_CODE);
-    }
-
-    // This callback is invoked when the Speech Recognizer returns.
-// This is where you process the intent and extract the speech text from the intent.
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode,
-                                    Intent data) {
-        if (requestCode == SPEECH_REQUEST_CODE && resultCode == RESULT_OK) {
-            List<String> results = data.getStringArrayListExtra(
-                    RecognizerIntent.EXTRA_RESULTS);
-            String spokenText = results.get(0);
-
-            List<String> partial = data.getStringArrayListExtra(
-                    RecognizerIntent.EXTRA_PARTIAL_RESULTS);
-            String partialText = partial.get(0);
-
-            textView.setText(partialText);
-
-            // Do something with spokenText
-
-            textView.setText(spokenText);
-        }
-
-
-        super.onActivityResult(requestCode, resultCode, data);
-    }
-
     private void speechInput() {
         Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
